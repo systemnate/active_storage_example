b54f04a5332c8a0e02ceb64321bafd774bde03cd
Nate Dalo <ndalo@merchantsbonding.com>
2021-03-01 15:06:09 -0600

    add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: 'user@example.com', password: 'password')
+Category.create(name: 'animals')
+Category.create(name: 'people')
+Category.create(name: 'places')
+Category.create(name: 'things')
