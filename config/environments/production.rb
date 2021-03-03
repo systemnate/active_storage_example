f18c720c04763052b92507b3731d47568b25ac0d
Nate Dalo <ndalo@merchantsbonding.com>
2021-03-03 11:12:06 -0600

    makes development and production S3 settings the same

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,12 +118,15 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.paperclip_defaults = {
-    storage: :s3,
+    storage:          :s3,
+    s3_permissions:   :private,
+    s3_region:        'us-east-2',
+    s3_host_name:     's3-us-east-2.amazonaws.com',
     s3_credentials: {
-      bucket: 'mbctestweb-activestorage',
       access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
       secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-      s3_region: 'us-east-2'
+      bucket: 'mbctestweb-activestorage',
+      region: 'us-east-2',
     }
   }
 end
